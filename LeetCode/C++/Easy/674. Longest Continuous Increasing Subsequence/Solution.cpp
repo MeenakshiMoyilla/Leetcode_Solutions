09ce6919ef1dcb30ceb4567405eb4381affb9c73
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-11 15:30:01 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/674. Longest Continuous Increasing Subsequence/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int count=1,maxi=1;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<nums[i+1]){
+                count++;
+                maxi=max(maxi,count);
+            }
+            else{
+                count=1;
+            }
+        }
+        return maxi;
+    }
+};
