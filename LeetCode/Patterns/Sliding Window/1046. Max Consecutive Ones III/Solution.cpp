b7b266f6e8821acce53269670690ad4a17f8f241
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-11 20:35:29 +0530

    Add C++ Solution to Patterns/Sliding Window

---
+++ LeetCode/Patterns/Sliding Window/1046. Max Consecutive Ones III/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int z=0,left=0,right=0,maxi=INT_MIN,a=-1;
+        while(right<nums.size() && z<=k){
+            if(nums[right]==1){
+                a=right-left+1; 
+                maxi=max(maxi,a);
+            }
+            else                z++;
+            right++;
+            if(z>k){
+                while(z>k){
+                    if(nums[left]==0)           z--;
+                    left++;
+                }
+            }
+        }
+        return maxi;
+    }
+};
