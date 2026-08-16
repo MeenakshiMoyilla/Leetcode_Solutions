60328b8d0a2849964f4a15d8a0edb6395b58f373
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-16 20:16:44 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/26. Remove Duplicates from Sorted Array/Solution.cpp
+++ LeetCode/C++/Easy/26. Remove Duplicates from Sorted Array/Solution.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i,j;
-        for(i=0;i<nums.size();i++)
+        int i,left=0,right=1;
+        while(right<nums.size())
         {
-            int freq=count(nums.begin(),nums.end(),nums[i]);
-            for(j=0;j<freq-1;j++)
+            if(nums[left]==nums[right]) 
             {
-                nums.erase(nums.begin()+i);
+                nums.erase(nums.begin()+right);
             }
-            // if(nums[i]==nums[i+1])
-            // {
-            //     nums.erase(nums.begin()+i);
-            //     // i++;
-            // }
+            else
+            {
+                left++;
+                right++;
+            }
         }
+        // for(i=0;i<nums.size();)
+        // {
+        //     if()
+        //     // int freq=count(nums.begin(),nums.end(),nums[i]);
+        //     // for(j=0;j<freq-1;j++)
+        //     // {
+        //     //     nums.erase(nums.begin()+i);
+        //     // }
+        //     // if(nums[i]==nums[i+1])
+        //     // {
+        //     //     nums.erase(nums.begin()+i);
+        //     //     // i++;
+        //     // }
+        // }
+        // cout<<nums.size();
         return nums.size();
     }
 };
