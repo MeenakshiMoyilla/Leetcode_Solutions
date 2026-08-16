376385059bfe3460a5bc0bd612cabaf5f5f5a563
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-16 17:52:54 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/88. Merge Sorted Array/Solution.cpp
+++ LeetCode/C++/Easy/88. Merge Sorted Array/Solution.cpp
@@ -8,7 +8,7 @@ public:
         }        
         else
         {
-            int i,j,count=0;
+            int i,j;
             int a=nums1.size();
             // cout<<nums1.size()<<endl;
             for(i=0;i<a;i++)
@@ -18,8 +18,6 @@ public:
                 if(nums1[i]==0)
                 {
                     nums1.erase(nums1.begin()+i);
-                    count++;
-                    if(count==n)    break;
                 }
                 // for(j=0;j<nums1.size();j++)
                 // {
