fa0b3b3852e1e7a4457e23c59ccce9e75bc22b24
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-16 19:32:44 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/409. Longest Palindrome/Solution.cpp
+++ LeetCode/C++/Easy/409. Longest Palindrome/Solution.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int i,res=0,count=0,maxi=INT_MIN;
+        int i,res=0,maxi=INT_MIN;
         unordered_map<char,int> mp;
         for(i=0;i<s.size();i++)            mp[s[i]]++;
         for(auto x:mp)        {
             if(x.second%2!=0)
-            {
                 if(maxi<x.second)   maxi=x.second;
-            }
-            if(x.second%2==0){
-                // if(x.second%2!=0)    count++;
-                res+=x.second;
-            }
+            if(x.second%2==0)       res+=x.second;
         }
         return res+maxi;
     }
