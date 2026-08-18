2162d095d32edb81f90fba9c2ecda2b17443ab96
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-18 20:00:10 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/53. Maximum Subarray/Solution.cpp
+++ LeetCode/C++/Medium/53. Maximum Subarray/Solution.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currentsum=nums[0];
-        int maxsum=nums[0];
-        for(int i=1;i<nums.size();i++)
+        int currentsum=nums[0],maxsum=nums[0],i;
+        for(i=1;i<nums.size();i++)
         {
             currentsum=max(currentsum+nums[i],nums[i]);
-            maxsum=max(maxsum, currentsum);
+            maxsum=max(maxsum,currentsum);
         }
         return maxsum;
     }
