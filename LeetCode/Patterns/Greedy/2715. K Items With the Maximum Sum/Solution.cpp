e056e3e62bcb3f3eed74838f12cd1ae3153e1c00
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-19 11:58:00 +0530

    Add C++ Solution to Patterns/Greedy

---
+++ LeetCode/Patterns/Greedy/2715. K Items With the Maximum Sum/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        // int a=numOnes+numZeros;
+        if(numOnes>=k)  return k;
+        if(numOnes+numZeros>=k) return numOnes;
+        return numOnes-(k-(numOnes+numZeros));
+        // if(k<=a)    return num;
+        // return a-((k-a)*-1);
+    }
+};
