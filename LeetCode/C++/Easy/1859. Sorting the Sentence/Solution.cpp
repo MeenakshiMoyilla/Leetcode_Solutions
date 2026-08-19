f466213592f527ae285eef17cfca31c9d4cb525f
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-19 12:40:25 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/1859. Sorting the Sentence/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isnum(char c)
+    {
+        return c>='0' && c<='9';
+    }
+    string sortSentence(string s) {
+        map<int,string> mp;
+        string s1;
+        for(int i=0;i<s.size();i++)
+        {
+            if(isnum(s[i]))
+            {
+                mp[s[i]-'0']=s1;
+                s1.clear();
+            }
+            else if(!(s[i]==' '))    s1.push_back(s[i]);
+            // else
+            // {
+            //     if(!s[i]==' ')          s1.push_back(s[i]);
+            // }        
+        }
+        s1.clear();
+        for(auto x: mp)
+        {
+            cout<<x.first<<" "<<x.second<<endl;
+            s1+=x.second;
+            s1+=' ';
+        }
+        s1.pop_back();
+        return s1;
+    }
+};
