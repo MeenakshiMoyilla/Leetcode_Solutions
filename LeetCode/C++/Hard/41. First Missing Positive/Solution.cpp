cc2f385e9a576f52e243414bcafd51023fba5dd9
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-24 21:53:50 +0530

    Add Hard C++ Solution from LeetCode

--- LeetCode/C++/Hard/41. First Missing Positive/Solution.cpp
+++ LeetCode/C++/Hard/41. First Missing Positive/Solution.cpp
@@ -4,7 +4,7 @@ public:
         int i,maxi=INT_MIN;
         unordered_map<int,int> mp;
         for(i=0;i<nums.size();i++)          mp[nums[i]]++;
-        for(i=1;i<=nums.size();i++)
+        for(i=1;i<=nums.size()+2;i++)
             if(!(mp.find(i)!=mp.end()))  return i;
         return 1;
     }
