d26b1d6288e9c1e31dbcaa3db4bec43d495c3747
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-24 21:53:20 +0530

    Add C++ Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/41. First Missing Positive/Solution.cpp
+++ LeetCode/Patterns/Hash Map & Hashing/41. First Missing Positive/Solution.cpp
@@ -3,20 +3,9 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int i,maxi=INT_MIN;
         unordered_map<int,int> mp;
-        for(i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-            // maxi=max(maxi,nums[i]);
-        }
-        // for(auto x: mp)
-        // {
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
+        for(i=0;i<nums.size();i++)          mp[nums[i]]++;
         for(i=1;i<=nums.size();i++)
-        {
-            cout<<i<<"  ";
             if(!(mp.find(i)!=mp.end()))  return i;
-        }
         return 1;
     }
 };
