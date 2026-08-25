59407de6451f8e1111eef9c97c268b4fbecd4e75
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-25 15:30:41 +0530

    Add C++ Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/35. Search Insert Position/Solution.cpp
+++ LeetCode/Patterns/Binary Search/35. Search Insert Position/Solution.cpp
@@ -3,46 +3,32 @@ class Solution
     public:
     int searchInsert(vector<int>& nums, int target) 
     {
-        int low=0,high=nums.size()-1;
+        int mid,low=0,high=nums.size()-1;  
         auto a=find(nums.begin(),nums.end(),target);
-        // cout<<a;
-        if(a!=nums.end())
-        {
+        // if(a!=nums.end())
+        // {    
         while(low<=high)
         {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-                break;
-            }
-            else if(nums[mid]<target)
-            low=mid+1;
-            else
-            {
-                high=mid-1;
-            }
-        } 
-        }
-        else 
-        {
-            while(low<=high)
-            {
-                int mid=low+(high-low)/2;
-                if(nums[mid]==target)
-                {
-                    return mid;
-                    break;
-                }
-                else if(nums[mid]<target)
-                low=mid+1;
-                else
-                {
-                    high=mid-1;
-                }
-            } 
-            return 1;    
-        }
-        return 0; 
+            mid=low+(high-low)/2;
+            if(nums[mid]==target)     return mid;
+            else if(nums[mid]<target)    low=mid+1;
+            else    high=mid-1;
+        }  
+        return low;
+        // }
+        // else
+        // {
+        //     if(target<nums[0]) return 0;
+        //     else if(target>nums[nums.size()-1]) return nums.size();
+        //     else
+        //     {
+        //     for(int i=1;i<nums.size();i++)
+        //     {
+        //         if(target>nums[i-1] && target<nums[i]);
+        //         return i;
+        //     }
+        //     }
+        // }
+        // return 0;
     }
 };
