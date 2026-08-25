25dba6a363a6695ed1e5c56afdbe238e9aa115af
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-25 14:48:33 +0530

    Add C++ Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/1206. Corporate Flight Bookings/Solution.cpp
+++ LeetCode/Patterns/Prefix Sum/1206. Corporate Flight Bookings/Solution.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> v(n,0);
-        int i,j,s=bookings.size();
-        for(i=0;i<s;i++)
+        int i,j;
+        for(i=0;i<bookings.size();i++)
         {
             for(j=bookings[i][0];j<=bookings[i][1];j++)
             {
