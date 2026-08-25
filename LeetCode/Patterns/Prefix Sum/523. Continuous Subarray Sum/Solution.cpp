e46c51beed2130fb4e5c596915482de395343e53
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-25 12:30:27 +0530

    Add C++ Solution to Patterns/Prefix Sum

---
+++ LeetCode/Patterns/Prefix Sum/523. Continuous Subarray Sum/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        vector<int> pre(nums.size()+1);
+        unordered_map<int,int> mp;
+        int i;
+
+        for(i=0;i<nums.size();i++)
+        {
+            pre[i+1]=pre[i]+nums[i];
+        }
+        mp[0]=1;
+        for(i=1;i<pre.size();i++)
+        {
+            int need=pre[i]%k;
+            auto it=mp.find(need);
+            if(it!=mp.end())    return true;
+        }
+        return false;
+    }
+};
