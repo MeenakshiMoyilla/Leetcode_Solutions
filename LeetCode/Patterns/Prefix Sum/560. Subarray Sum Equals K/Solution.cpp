964d9512784dcb95820bc0c96798ded61aef8f38
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-25 10:07:47 +0530

    Add C++ Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.cpp
+++ LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.cpp
@@ -5,20 +5,12 @@ public:
         pre[0]=0;
         unordered_map<int,int> mp;
         int i,res=0;
-        for(i=0;i<nums.size();i++)
-        {
-            pre[i+1]=pre[i]+nums[i];
-        }
+        for(i=0;i<nums.size();i++)       pre[i+1]=pre[i]+nums[i];
         mp[0]=1;
-        for(i=1;i<pre.size();i++)
-        {
-            // cout<<pre[i]<<endl;
+        for(i=1;i<pre.size();i++){
             int need=pre[i]-k;
             auto it=mp.find(need);
-            if(it!=mp.end())
-            {
-                res=res+mp[need];
-            }
+            if(it!=mp.end())              res=res+mp[need];
             mp[pre[i]]++;
         }
         return res;
