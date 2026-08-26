56290575aa86ba62657cee05ab5abbd06ce93b47
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-26 17:34:29 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/448. Find All Numbers Disappeared in an Array/Solution.cpp
+++ LeetCode/C++/Easy/448. Find All Numbers Disappeared in an Array/Solution.cpp
@@ -5,11 +5,10 @@ public:
         vector<int> v;
         int i;
 
-        for(i=0;i<nums.size();i++)     mp[nums[i]]++;
+        for(i=0;i<nums.size();i++)           mp[nums[i]]++;
 
         for(i=1;i<=nums.size();i++){
-            auto it=mp.find(i);
-            if(!(it!=mp.end()))        v.push_back(i);
+            if(mp.find(i)==mp.end())         v.push_back(i);
         }
         return v;
     }
