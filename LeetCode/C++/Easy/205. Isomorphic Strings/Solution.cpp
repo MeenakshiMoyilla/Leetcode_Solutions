6ea7861294ae59d2508ec6215046588700565314
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-27 19:23:47 +0530

    Add Easy C++ Solution for 205. Isomorphic Strings from LeetCode

--- LeetCode/C++/Easy/205. Isomorphic Strings/Solution.cpp
+++ LeetCode/C++/Easy/205. Isomorphic Strings/Solution.cpp
@@ -4,15 +4,12 @@ public:
         unordered_map<char,char> mp;
         unordered_map<char,char> mp1;
         int i;
-        for(i=0;i<s.size();i++)
-        {
-            mp[s[i]]=t[i];
-            mp1[t[i]]=s[i];
+        for(i=0;i<s.size();i++)  {
+            if(mp.find(s[i])==mp.end())       mp[s[i]]=t[i];
+            if(mp1.find(t[i])==mp1.end())        mp1[t[i]]=s[i];
         }
-        for(auto x: mp)
-        cout<<x.first<<" "<<x.second<<endl;
-        for(i=0;i<s.size();i++)
-        {
+
+        for(i=0;i<s.size();i++) {
             if(t[i]==mp[s[i]] && s[i]==mp1[t[i]]) {}
             else        return false;
         }
