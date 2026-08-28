dbe19a8a888b46353fd64c6fb6d52d1d9040680b
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-28 11:59:05 +0530

    Add Easy C++ Solution for 389. Find the Difference from LeetCode

--- LeetCode/C++/Easy/389. Find the Difference/Solution.cpp
+++ LeetCode/C++/Easy/389. Find the Difference/Solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t){
-        int i;
-        unordered_map<char,int> mp;
-        unordered_map<char,int> mp1;
-        for(i=0;i<s.size();i++)
+    char findTheDifference(string s, string t)
+    {
+        // char a='a';
+        int len=s.size(),i;
+        for(i=0;i<len;i++)
         {
-            mp[s[i]]++;
-            mp1[t[i]]++;
+            int x=t.find(s[i]);
+            t.erase(t.begin()+x);
         }
-        mp1[t[t.size()-1]]++;
-        for(auto x: mp1)
-        {
-            if(mp.find(x.first)==mp.end())  return x.first;
-        }
-        return 'a';
+        // cout<<t;
+        // char a=t[0];
+        return t[0];
     }
 };
--- LeetCode/Patterns/Bit Manipulation/389. Find the Difference/README.md
+++ LeetCode/Patterns/Bit Manipulation/389. Find the Difference/README.md
@@ -8,8 +8,8 @@
 Hash Table, String, Bit Manipulation, Sorting
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 0 ms
+- **Memory:** 9.2 MB
 
 ---
 
