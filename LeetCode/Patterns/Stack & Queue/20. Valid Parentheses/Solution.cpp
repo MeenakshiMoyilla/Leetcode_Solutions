5196633a38e7fd28c9b1f70fe03c0a20b3e09de6
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-28 19:20:41 +0530

    Add Easy C++ Solution for 20. Valid Parentheses from LeetCode

--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.cpp
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.cpp
@@ -6,47 +6,33 @@ public:
         stack<char> st;
         if(s[0]==')' || s[0]=='}' ||  s[0]==']')
         return false;
-        else
-        {
-            for(i=0;i<s.size();i++)
-            {
-                if(s[i]=='(' || s[i]=='[' || s[i]=='{')
-                {
-                    st.push(s[i]);
-                // break;
-                }
-                if(st.empty())
-                {
+
+        else{
+            for(i=0;i<s.size();i++){
+                if(s[i]=='(' || s[i]=='[' || s[i]=='{')    st.push(s[i]);
+
+                if(st.empty()){
                     if(s[i]==']' || s[i]=='}' || s[i]==')')
                     return false;
                 }
 
-                if(s[i]==')')
-                {
-                    if(st.top()=='(')      st.pop();
-                    else return false;
-                }          
-                if(s[i]==']')
-                {
+                if(s[i]==')'){
+                    if(st.top()=='(')       st.pop();
+                    else                    return false;
+                }   
+
+                if(s[i]==']'){
                     if(st.top()=='[')      st.pop(); 
-                    else return false;
+                    else                   return false;
                 }
-                if(s[i]=='}')
-                {
+
+                if(s[i]=='}'){
                     if(st.top()=='{')      st.pop();
-                    else    return false;
+                    else                   return false;
                 }
             }
             if(st.empty())          return true;
-            else
-            {
-            //  while(!st.empty()) 
-            // {
-            //     cout << st.top() << " ";
-            //     st.pop();
-            // }                    
-                return false;
-            } 
+            else                    return false;
         } 
               
     }
