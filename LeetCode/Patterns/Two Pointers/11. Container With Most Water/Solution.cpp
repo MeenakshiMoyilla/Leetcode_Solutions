473eb3a518a70647fa72f03e0e24e721fe846445
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-08-30 20:00:26 +0530

    Add Medium C++ Solution for 11. Container With Most Water from LeetCode

--- LeetCode/Patterns/Two Pointers/11. Container With Most Water/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/11. Container With Most Water/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left=0,right=height.size()-1,mini=INT_MAX,maxi=1,current=0;
+        int left=0,right=height.size()-1,mini=INT_MAX,maxi=0,current=0;
         while(left<right && right<height.size())
         {
             mini=min(height[left],height[right]);
