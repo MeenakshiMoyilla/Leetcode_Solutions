6aa58a1bbe7b583ab8d32fd01c058eaa213f8f3e
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-02 19:35:45 +0530

    Add Easy C++ Solution for 2102. Find the Middle Index in Array from LeetCode

---
+++ LeetCode/Patterns/Prefix Sum/2102. Find the Middle Index in Array/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        vector<int> v1(nums.size());
+        vector<int> v2(nums.size());
+        v1[0]=0,v2[nums.size()-1]=0;
+        int i;
+        for(i=1;i<nums.size();i++)
+        {
+            v1[i]=v1[i-1]+nums[i-1];
+            v2[nums.size()-1-i]=v2[nums.size()-i]+nums[nums.size()-i];
+        }
+        for(i=0;i<nums.size();i++)
+        {
+            if(v1[i]==v2[i])    return i;
+        }
+        return -1;
+    }
+};
