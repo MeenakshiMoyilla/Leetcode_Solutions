fb747a3792880c0a2b860e27db178bc1b1fe385b
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-03 11:50:39 +0530

    Add Easy C++ Solution for 88. Merge Sorted Array from LeetCode

--- LeetCode/Patterns/Two Pointers/88. Merge Sorted Array/Solution.cpp
+++ LeetCode/Patterns/Two Pointers/88. Merge Sorted Array/Solution.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
-    {
-        if(n==0)
-        {
-            sort(nums1.begin(),nums1.end());
-        }        
-        else
-        {
-            int i,j,count=0;
-            int a=nums1.size();
-            // cout<<nums1.size()<<endl;
-            for(i=0;i<a;i++)
-            {
-                // cout<<i<<"  ";
-                // cout<<endl<<nums1[i]<<endl;
-                if(nums1[i]==0)
-                {
-                    nums1.erase(nums1.begin()+i);
-                    count++;
-                    if(count==n)    break;
-                }
-                // for(j=0;j<nums1.size();j++)
-                // {
-                //     cout<<nums1[j]<<"  ";
-                // }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i;
+        for(i=m;i<m+n;i++)    nums1[i]=nums2[i-m]; 
+        
+        int left=0,right=m;
+        while(left<right && right<m+n){
+            if(nums1[right]<nums1[left]){
+                swap(nums1[right],nums1[left]);
+                left++;
+                right++;
             }
-            // cout<<endl;
-            for(i=0;i<nums2.size();i++)
-            {
-                nums1.push_back(nums2[i]);
-            }
-            sort(nums1.begin(),nums1.end());
+            else              left++;        
         }
+        
     }
 };
