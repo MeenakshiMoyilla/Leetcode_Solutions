e42d7162392ae074c1c32c191a810363e336d7db
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-07 22:21:19 +0530

    Add Easy C++ Solution for 844. Backspace String Compare from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/844. Backspace String Compare/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> st1;
+        stack<char> st2;
+        int i;
+        for(i=0;i<s.size();i++)
+        {
+            if(s[i]=='#')
+            st1.pop();
+            else
+            st1.push(s[i]);
+        }
+        for(i=0;i<t.size();i++)
+        {
+            if(t[i]=='#')
+            st2.pop();
+            else
+            st2.push(s[i]);
+        }
+        return st1==st2;
+    }
+};
