680dd313d76a828aeb271e74a3073149778c5731
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-08 14:28:32 +0530

    Add Easy C++ Solution for 682. Baseball Game from LeetCode

--- LeetCode/Patterns/Stack & Queue/682. Baseball Game/Solution.cpp
+++ LeetCode/Patterns/Stack & Queue/682. Baseball Game/Solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    // bool()
     int calPoints(vector<string>& operations) {
         stack<string> st;
         int i,res=0;
 
         for(i=0;i<operations.size();i++)
         {
-            if(operations[i][0]=='D')
-            {
+            if(operations[i][0]=='D'){
                 string a=st.top();
                 int num=stoi(a)*2;
                 string b=to_string(num);
                 st.push(b);
             }
-            else if(operations[i][0]=='C')
-            {
+            else if(operations[i][0]=='C'){
                 st.pop();
             }
-            else if(operations[i][0]=='+')
-            {
+            else if(operations[i][0]=='+'){
                 string a = st.top();
                 st.pop();
                 string b = st.top();
@@ -28,8 +24,7 @@ public:
                 st.push(a);
                 st.push(c);
             }
-            else
-            {
+            else{
                 st.push(operations[i]);
             }
         }
