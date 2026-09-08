985be97012c5bd7fb488ece0960d81fa0d3d91b9
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-08 14:09:19 +0530

    Add Easy C++ Solution for 1128. Remove All Adjacent Duplicates In String from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/1128. Remove All Adjacent Duplicates In String/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        string res;
+        st.push(s[0]);
+        int i;
+        for(i=1;i<s.size();i++)
+        {
+            if(!st.empty() && s[i]==st.top())
+            st.pop();
+            else
+            st.push(s[i]);
+        }
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
