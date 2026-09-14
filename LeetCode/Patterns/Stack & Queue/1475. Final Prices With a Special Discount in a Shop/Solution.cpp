2a2cf8365773c14558c2b9b532d946dad7346064
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-14 20:46:52 +0530

    Add Easy C++ Solution for 1475. Final Prices With a Special Discount in a Shop from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/1475. Final Prices With a Special Discount in a Shop/Solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int i,f=0,n=prices.size();
+        // vector<int> nse(n,-1);
+        vector<int> ans(n,-1);
+        stack<int> st;
+        st.push(prices[n-1]);
+        for(i=n-2;i>=0;i--)
+        {
+            f=0;
+            if(st.top()<=prices[i])
+            {
+                ans[i]=st.top();
+                
+            }
+            else
+            {
+                while(!st.empty() && f==0)
+                {
+                    if(st.top()<=prices[i])
+                    {
+                        ans[i]=st.top();
+                        f++;
+                    }
+                    else
+                    {
+                        st.pop();
+                    }
+                }
+            }
+            st.push(prices[i]);
+        }
+        for(i=0;i<n;i++)
+        {
+            if(ans[i]!=-1)
+            ans[i]=prices[i]-ans[i];
+            else
+            ans[i]=prices[i];
+        }
+        return ans;
+    }
+};
