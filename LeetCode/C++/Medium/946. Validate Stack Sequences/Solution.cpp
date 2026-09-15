bd35c924f9abcc634f64e998f008e3208de7f892
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-15 12:30:09 +0530

    Add Medium C++ Solution for 946. Validate Stack Sequences from LeetCode

--- LeetCode/C++/Medium/946. Validate Stack Sequences/Solution.cpp
+++ LeetCode/C++/Medium/946. Validate Stack Sequences/Solution.cpp
@@ -3,27 +3,17 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         int i,j=0;
         stack<int> st;
-        // st.push(pushed[0]);
-        // if(popped[0]==st.top()){
-        // st.pop();
-        // j++;
-        // }
-        for(i=0;i<pushed.size();i++)
-        {
+
+        for(i=0;i<pushed.size();i++){
             st.push(pushed[i]);
-            if(!st.empty() && st.top()==popped[j])
-            {
+            while(!st.empty() && st.top()==popped[j]){
                 st.pop();
                 j++;
             }
         }
-        // cout<<popped[j]<<" "<<st.top();
-        for(i=j;i<popped.size();i++)
-        {
+        for(i=j;i<popped.size();i++){
             if(!st.empty() && st.top()==popped[i])
-            {
                 st.pop();                
-            }
         }
         
         return st.empty();
