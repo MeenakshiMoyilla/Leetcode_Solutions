1e75250694f7633afd0739dfcfdcf7f6bb8e2a80
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-16 19:54:54 +0530

    Add Medium C++ Solution for 1249. Minimum Remove to Make Valid Parentheses from LeetCode

---
+++ LeetCode/C++/Medium/1249. Minimum Remove to Make Valid Parentheses/Solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<char> st;
+        int i,open=0,close=0;
+        // stack<char> st2;
+        for(i=0;i<s.size();i++)
+        {
+            if(s[i]==')')
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
+                else
+                {
+                    close++;
+                }
+            }
+            else if(s[i]=='(')
+            {
+                st.push(')');
+            }
+        }
+        open=st.size();
+        for(i=0;i<s.size();)
+        {
+            if(s[i]=='(' && open>0)
+            {
+                s.erase(s.begin()+i);
+                open--;
+            }
+            else if(s[i]==')' && close>0)
+            {
+                s.erase(s.begin()+i);
+                close--;
+            }
+            else i++;
+        }
+        return s;
+    }
+};
