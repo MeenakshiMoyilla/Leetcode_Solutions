737d68b9cd5260e7edc58310b993abc11b4716c4
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-19 14:15:52 +0530

    Add Medium C++ Solution for 1046. Max Consecutive Ones III from LeetCode

--- LeetCode/C++/Medium/1046. Max Consecutive Ones III/Solution.cpp
+++ LeetCode/C++/Medium/1046. Max Consecutive Ones III/Solution.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int z=0,left=0,right=0,maxi=0,a=0;
-        while(right<nums.size() && z<=k){
-            if(nums[right]==1){
-                a=right-left+1; 
-                maxi=max(maxi,a);
-            }
-            else                z++;
-            right++;
-            if(z>k){
-                while(z>k){
-                    if(nums[left]==0)           z--;
+        int right=0,left=0,maxi=0,sum=0,n=nums.size();
+        unordered_map<int,int> mp;
+
+        for(right=0;right<n;right++)
+        {
+            mp[nums[right]]++;
+            sum++;
+            if(sum-mp[1]<=k)           maxi=max(maxi,right-left+1);
+            else
+            {
+                while(mp[0]>k)
+                {
+                    mp[nums[left]]--;
                     left++;
+                    sum--;
                 }
             }
         }
