cf1e19e16fdffa7b715d61546d88573b5f6a2ea0
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-20 19:25:45 +0530

    Add Easy C++ Solution for 2020. Remove One Element to Make the Array Strictly Increasing from LeetCode

---
+++ LeetCode/C++/Easy/2020. Remove One Element to Make the Array Strictly Increasing/Solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        stack<int> st;
+        stack<int> st2;
+        st.push(nums[0]);
+        int i,n=nums.size(),a=0,b=0;
+        st2.push(nums[n-1]);
+        
+        for(i=1;i<n;i++){
+            while(!st.empty() && nums[i]<=st.top()){
+                st.pop();
+                a++;
+            }
+            st.push(nums[i]);
+        }
+
+        for(i=n-2;i>=0;i--){
+                while(!st2.empty() && nums[i]>=st2.top()){
+                st2.pop();
+                b++;
+            }
+            st2.push(nums[i]);
+        }
+
+        if(a>1 && b>1)  return false;
+
+        return true;
+    }
+};
