4d853383ab3fcdb42ae9bae7d8f1ae503d852540
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-20 20:25:51 +0530

    Add Easy C++ Solution for 3617. Find the Original Typed String I from LeetCode

---
+++ LeetCode/Patterns/Strings/3617. Find the Original Typed String I/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        unordered_map<char,int> mp;
+        int i,res=1,n=word.size();
+        mp[word[0]]++;
+        for(i=1;i<n;i++)
+        {
+            if(word[i]==word[i-1])          mp[word[i]]++;
+            else{
+                res=res+(mp[word[i-1]]-1);
+                mp.erase(word[i-1]);
+                mp[word[i]]++;
+            }
+        }
+
+        res=res+(mp[word[n-1]]-1);
+        
+        return res;
+    }
+};
