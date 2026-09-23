65cc33f99e129e32e8e396b9059caac6998a5ede
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-23 19:14:58 +0530

    Add Medium C++ Solution for 402. Remove K Digits from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/402. Remove K Digits/Solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string s;
+        stack<int> st;
+        int i,cnt=0;
+        for(i=0;i<num.size();i++)
+        {
+            if(!st.empty() && cnt<k)
+            {
+                int a=num[i]-'0';
+                if(st.top()>a ){
+                st.pop();
+                st.push(a);
+                cnt++;
+                }
+                else st.push(a);
+            }
+            else
+            {
+               
+                int a=num[i]-'0';
+                st.push(a);
+            }
+        }
+        while(!st.empty())
+        {
+            cout<<st.top()<<"  ";
+            // int res=res*st.top()
+            s+=to_string(st.top());
+            st.pop();
+        }
+        reverse(s.begin(),s.end());
+        i=0;
+        if(s.size()>1){
+        for(i=0;i<s.size();)
+        {
+            if(s[i]-'0'==0)
+            {
+                s.erase(s.begin()+i);
+            }
+            else break;
+        }
+        }
+
+        return s;
+    }
+};
