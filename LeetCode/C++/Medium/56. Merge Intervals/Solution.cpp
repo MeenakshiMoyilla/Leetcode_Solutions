c9c65bac8434551f2372dc7a491003cdc904cb58
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-24 13:25:27 +0530

    Add Medium C++ Solution for 56. Merge Intervals from LeetCode

--- LeetCode/C++/Medium/56. Merge Intervals/Solution.cpp
+++ LeetCode/C++/Medium/56. Merge Intervals/Solution.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-        int i,n=intervals.size();
+        int i,n=intervals.size(),start,end;
         vector<int> current=intervals[0];
         vector<vector<int>> ans;
-        // return current;
-        // cout<<current[0]<<endl;
-        // cout<<current[1];
-        for(i=1;i<n;i++)
-        {
-            if(current[1]>=intervals[i][0])
+
+        for(i=1;i<n;i++){
+            start=max(current[0],intervals[i][0]);
+            end=min(current[1],intervals[i][1]);
+            if(start<=end)
             {
+                if(current[1]<intervals[i][1])
                 current[1]=intervals[i][1];
             }
             else
             {
                 ans.push_back(current);
-                current[0]=intervals[i][0];
-                current[1]=intervals[i][1];
+                current=intervals[i];
             }
-
         }
         ans.push_back(current);
         return ans;
