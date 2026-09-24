e4628ecebf4ca1116fc5419c9f367490529bfe17
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-24 13:46:02 +0530

    Add Medium C++ Solution for 986. Interval List Intersections from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/986. Interval List Intersections/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        int i=0,j=0,m=firstList.size(),n=secondList.size(),start=2,end=1;
+        vector<vector<int>> ans;
+
+        while(i<m && j<n)
+        {
+            start=max(firstList[i][0],secondList[j][0]);
+            end=min(firstList[i][1],secondList[j][1]);
+            if(start<=end)            {
+                ans.push_back({start,end});
+            }
+            if(firstList[i][1]<secondList[j][1])        i++;
+            else j++;
+        }
+
+        return ans;
+    }
+};
