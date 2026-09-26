13846d82b771eb8d4ede193ddb84f4d5887db462
MeenakshiMoyilla <meenakshi.moyilla@gmail.com>
2026-09-26 06:32:40 +0530

    Add Easy C++ Solution for 1802. Number of Students Unable to Eat Lunch from LeetCode

---
+++ LeetCode/C++/Easy/1802. Number of Students Unable to Eat Lunch/Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int i,frnt=0,a=0;
+        queue<int> stu;
+        for(i=0;i<students.size();i++)
+        {
+            stu.push(students[i]);
+        }
+        for(i=0;i<sandwiches.size();)
+        {
+            if(sandwiches[i]==stu.front())
+            {
+                stu.pop();
+                i++;
+                a=0;
+            }
+            else
+            {
+                int x=stu.front();
+                stu.pop();
+                stu.push(x);  
+                a++;
+                if(a==stu.size())    break;              
+            }
+        }
+        return stu.size();
+    }
+};
